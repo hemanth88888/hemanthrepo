341ac4a83ed380ccaff163a232a80df9811ec963
hemanth88888 <hemanth88888gmail.com>
2016-07-20 11:29:08 +0530

    first commit

---
+++ Sample99/src/main/java/BusinessLogic/App.java
@@ -0,0 +1,10 @@
+package BusinessLogic;
+
+import org.testng.annotations.Test;
+
+public class App {
+  @Test
+  public void f() {
+	  System.out.println("hello sample in APP src main5555");
+  }
+}
---
+++ Sample99/src/test/java/BusinessTest/AppTest.java
@@ -0,0 +1,10 @@
+package BusinessTest;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+  @Test
+  public void f() {
+	  System.out.println("hello sample in APPtest src test9999");
+  }
+}
